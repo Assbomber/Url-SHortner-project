8e2d9042f4c4b9c538affaad870f09adfcb8133b
Aman kumar <36842116+Assbomber@users.noreply.github.com>
2019-11-10 10:38:07 +0530

    Create auth.php
    Connection file

---
+++ auth.php
@@ -0,0 +1,8 @@
+<?php
+
+$username="u847003778_amanu";
+                 $password="Anurag8559@";
+                 $database="u847003778_records";
+
+                 $database=new mysqli("localhost",$username,$password,$database) or die("Error connecting to Database!");
+?>
